bfc055e2fea3423da3656e0104adbe662fc2a431
Artyom <artemgorbachev046@gmail.com>
2023-02-11 23:47:38 +0600

    Added Card permant select

--- Assets/Scripts/UI/Selectables/Cards/CardRaycaster.cs
+++ Assets/Scripts/UI/Selectables/Cards/CardRaycaster.cs
@@ -14,51 +14,106 @@ namespace Assets.Scripts.UI.Selectables.Cards
         [SerializeField] private LayerMask _layerMask;
         [SerializeField] private InputAction _mouseAction;
 
+        [SerializeField] private InputAction _selectAction;
+
+        private bool _permantSelect;
         private ISelectable _currentSelectable;
+
+        private readonly List<RaycastResult> _results = new();
 
         private void Start()
         {
-            var results = new List<RaycastResult>();
             var eventSystem = EventSystem.current;
 
             Observable.EveryUpdate().Subscribe(x =>
             {
-                results.Clear();
+                if (_permantSelect) return;
 
-                var pointerData = new PointerEventData(eventSystem)
+                if (TryRaycastSelectable(out var selectable))
                 {
-                    position = _mouseAction.ReadValue<Vector2>()
-                };
+                    Deselect(_currentSelectable);
 
-                _raycaster.Raycast(pointerData, results);
-
-                foreach (RaycastResult result in results)
-                {
-                    var resultObject = result.gameObject;
-
-                    if ((_layerMask & (1 << resultObject.layer)) == 0 
-                    || !resultObject.TryGetComponent<ISelectable>(out var selectable))
-                        continue;
-                    
-                    DeselectCurrent();
-
-                    selectable.Select();
-                    _currentSelectable = selectable;
+                    Select(selectable);
 
                     return;
                 }
 
-                DeselectCurrent();
+                Deselect(_currentSelectable);
+
             }).AddTo(this);
 
+            _selectAction.performed += (arg) =>
+            {
+                if (TryRaycastSelectable(out var selectable))
+                {
+                    if (_permantSelect)
+                    {
+                        if (selectable == _currentSelectable)
+                        {
+                            _permantSelect = false;
+                            Deselect(selectable);
+
+                            return;
+                        }
+                        else
+                        {
+                            Deselect(_currentSelectable);
+
+                            Select(selectable);
+                        }
+                    }
+
+                    if (!_permantSelect)
+                    {
+                        _permantSelect = true;
+                        Select(selectable);
+                    }
+                }
+            };
+
             _mouseAction.Enable();
+            _selectAction.Enable();
         }
 
-        private void DeselectCurrent()
+        public bool TryRaycastSelectable(out ISelectable selectable)
         {
-            if (_currentSelectable == null) return;
+            _results.Clear();
+            selectable = null;
 
-            _currentSelectable.Deselect();
+            var pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = _mouseAction.ReadValue<Vector2>()
+            };
+
+            _raycaster.Raycast(pointerData, _results);
+
+            foreach (RaycastResult result in _results)
+            {
+                var resultObject = result.gameObject;
+
+                if ((_layerMask & (1 << resultObject.layer)) == 0
+                || !resultObject.TryGetComponent(out selectable))
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Select(ISelectable selectable)
+        {
+            if (selectable == null) return;
+
+            selectable.Select();
+            _currentSelectable = selectable;
+        }
+
+        private void Deselect(ISelectable selectable)
+        {
+            if (selectable == null) return;
+
+            selectable.Deselect();
             _currentSelectable = null;
         }
     }
