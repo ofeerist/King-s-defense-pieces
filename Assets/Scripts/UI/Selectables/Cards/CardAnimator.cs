c111aeb7216c55f984199e09df2afe13c5ea9a21
Artyom <artemgorbachev046@gmail.com>
2023-02-11 23:21:31 +0600

    Added card appear animation

--- Assets/Scripts/UI/Selectables/Cards/CardAnimator.cs
+++ Assets/Scripts/UI/Selectables/Cards/CardAnimator.cs
@@ -1,5 +1,8 @@
-﻿using UniRx;
+﻿using Assets.Scripts.Utils;
+using System.Collections;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI.Selectables.Cards
 {
@@ -7,6 +10,9 @@ namespace Assets.Scripts.UI.Selectables.Cards
     {
         [SerializeField] private RectTransform _rectToExpand;
         [SerializeField] private RectTransform _rectToScale;
+
+        [SerializeField] private LayoutElement _layoutElement;
+        [SerializeField] private float _startAnimationDuration;
 
         [SerializeField] private float _targetMultiplier;
         [SerializeField] private float _lerpSpeed;
@@ -17,35 +23,81 @@ namespace Assets.Scripts.UI.Selectables.Cards
         private Vector3 _startScale;
         private Vector3 _targetScale;
 
+        private float _startScaleY;
+        private float _targetScaleY;
+
         private bool _isSelected;
 
         private void Start()
         {
-            _startSize = _rectToExpand.sizeDelta;
+            _startSize = new Vector2(_layoutElement.preferredWidth, _layoutElement.preferredHeight);
             _targetSize = _startSize * _targetMultiplier;
 
             _startScale = _rectToScale.localScale;
             _targetScale = _startScale * _targetMultiplier;
 
-            Observable.EveryUpdate().Subscribe(x =>
+            _startScaleY = 0;
+            _targetScaleY = (_startSize.y * _targetMultiplier - _startSize.y) / 2f;
+
+            Observable.FromMicroCoroutine(ShowSelf).Subscribe(x =>
             {
-                if (_isSelected)
+                Observable.EveryUpdate().Subscribe(x =>
                 {
-                    _rectToExpand.sizeDelta = 
-                        Vector2.Lerp(_rectToExpand.sizeDelta, _targetSize, _lerpSpeed * Time.deltaTime);
+                    var speed = _lerpSpeed * Time.deltaTime;
 
-                    _rectToScale.localScale =
-                        Vector3.Lerp(_rectToScale.localScale, _targetScale, _lerpSpeed * Time.deltaTime);
-                }
-                else
-                {
-                    _rectToExpand.sizeDelta =
-                        Vector2.Lerp(_rectToExpand.sizeDelta, _startSize, _lerpSpeed * Time.deltaTime);
+                    if (_isSelected)
+                    {
+                        LerpLayoutSize(_layoutElement, _targetSize, speed);
 
-                    _rectToScale.localScale =
-                        Vector3.Lerp(_rectToScale.localScale, _startScale, _lerpSpeed * Time.deltaTime);
-                }
+                        _rectToScale.localScale =
+                            Vector3.Lerp(_rectToScale.localScale, _targetScale, speed);
+
+                        _rectToScale.anchoredPosition = new Vector2(_rectToScale.anchoredPosition.x,
+                            Mathf.Lerp(_rectToScale.anchoredPosition.y, _targetScaleY, speed));
+                    }
+                    else
+                    {
+                        LerpLayoutSize(_layoutElement, _startSize, speed);
+
+                        _rectToScale.localScale =
+                            Vector3.Lerp(_rectToScale.localScale, _startScale, speed);
+
+                        _rectToScale.anchoredPosition = new Vector2(_rectToScale.anchoredPosition.x,
+                            Mathf.Lerp(_rectToScale.anchoredPosition.y, _startScaleY, speed));
+                    }
+                }).AddTo(this);
             }).AddTo(this);
+        }
+
+        private void LerpLayoutSize(LayoutElement element, Vector2 targetSize, float speed)
+        {
+            element.preferredHeight = Mathf.Lerp(element.preferredHeight, targetSize.y, speed);
+            element.preferredWidth = Mathf.Lerp(element.preferredWidth, targetSize.x, speed);
+        }
+
+        private IEnumerator ShowSelf()
+        {
+            float startWidth = 0;
+            float endWidth = _rectToExpand.rect.width;
+
+            float startY = _rectToScale.anchoredPosition.y;
+            float endY = 0;
+
+            float time = 0.0f;
+            while (time < _startAnimationDuration)
+            {
+                _layoutElement.preferredWidth = 
+                    LinearAnimation.EaseInOut(startWidth, endWidth, time, _startAnimationDuration);
+
+                _rectToScale.anchoredPosition = new Vector2(_rectToScale.anchoredPosition.x,
+                    LinearAnimation.EaseInOut(startY, endY, time, _startAnimationDuration));
+
+                yield return null;
+                time += Time.deltaTime;
+            }
+
+            _rectToScale.anchoredPosition = new Vector2(_rectToScale.anchoredPosition.x, endY);
+            _layoutElement.preferredWidth = endWidth;
         }
 
         public void Deselect()
---
+++ Assets/Scripts/Utils/LinearAnimation.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Utils
+{
+    internal class LinearAnimation
+    {
+        public static float EaseInOut(float initial, float final, float time, float duration)
+        {
+            float change = final - initial;
+            time /= duration / 2;
+            if (time < 1f) return change / 2 * time * time + initial;
+            time--;
+            return -change / 2 * (time * (time - 2) - 1) + initial;
+        }
+    }
+}
