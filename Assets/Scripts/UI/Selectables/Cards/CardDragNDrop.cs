f3bbb3e7bbde4642a90f6f05f09bbf0ee26e5807
Artyom <artemgorbachev046@gmail.com>
2023-02-13 16:37:07 +0600

    Fixed Sharp Animation

--- Assets/Scripts/UI/Selectables/Cards/CardDragNDrop.cs
+++ Assets/Scripts/UI/Selectables/Cards/CardDragNDrop.cs
@@ -1,14 +1,25 @@
 ﻿using Assets.Scripts.Building;
+using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.UI.Selectables.Cards
 {
-    internal class CardDragNDrop : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+    internal class CardDragNDrop : MonoBehaviour, IPointerUpHandler, IDragHandler, IPointerDownHandler
     {
         [SerializeField] private BuildRaycaster _buildRaycaster;
+        [SerializeField] private GraphicRaycaster _raycaster;
+
+        [Space]
+
+        [SerializeField] private GameObject _cardHolderGameObject;
+
+        [SerializeField] private RectTransform _cardPrefabParent;
+        [SerializeField] private RectTransform _gridHolder;
+        [SerializeField] private RectTransform _cardHolder;
 
         [Space]
 
@@ -23,17 +34,64 @@ namespace Assets.Scripts.UI.Selectables.Cards
         public bool IsIso { get; set; }
 
         private readonly SerialDisposable _serialDisposable = new();
+        private readonly SerialDisposable _parentDisposable = new();
         private Transform _targetPosition;
 
         private BuildCell _currentCellSelectable;
+        private readonly List<RaycastResult> _results = new();
+
+        [SerializeField] private InputAction _mousePosition;
 
         private void Start()
         {
             _serialDisposable.AddTo(this);
+            _parentDisposable.AddTo(this);
         }
 
         public void OnDrag(PointerEventData eventData)
         {
+            if (IsRaycastCardHolder(eventData.position))
+            {
+                _cardAnimator.IsHand = true;
+                _cardPrefabParent.SetParent(_cardHolder, false);
+                _parentDisposable.Disposable = Observable.NextFrame().Subscribe(x =>
+                {
+                    _rectTransform.position = eventData.position;
+                });
+                _rectTransform.position = eventData.position;
+
+                var distance = _cardPrefabParent.position.x - eventData.position.x;
+
+                var index = _cardPrefabParent.GetSiblingIndex();
+                if (distance > 0 && distance > _cardPrefabParent.sizeDelta.x / 2)
+                {
+                    if (index - 1 < 0) return;
+
+                    _cardPrefabParent.SetSiblingIndex(index - 1);
+
+                    _parentDisposable.Disposable = Observable.NextFrame().Subscribe(x =>
+                    {
+                        _rectTransform.position = eventData.position; 
+                    });
+                    _rectTransform.position = eventData.position;
+                }
+
+                if (distance < 0 && distance < -_cardPrefabParent.sizeDelta.x / 2)
+                {
+                    if (index + 1 >= _cardHolder.childCount) return;
+
+                    _cardPrefabParent.SetSiblingIndex(_cardPrefabParent.GetSiblingIndex() + 1);
+
+                    _parentDisposable.Disposable = Observable.NextFrame().Subscribe(x =>
+                    {
+                        _rectTransform.position = eventData.position;
+                    });
+                    _rectTransform.position = eventData.position;
+                }
+
+                return;
+            }
+
             _cardAnimator.IsHand = false;
 
             IsIso = _buildRaycaster.TryRaycastBuildCellPosition(eventData.position, 
@@ -68,11 +126,37 @@ namespace Assets.Scripts.UI.Selectables.Cards
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            _cardAnimator.IsHand = true;
-            IsIso = false;
-            Deselect(_currentCellSelectable);
+            if (_cardAnimator.IsHand)
+            {
+                IsIso = false;
+                Deselect(_currentCellSelectable);
 
-            _rectTransform.anchoredPosition = Vector2.zero;
+                _rectTransform.anchoredPosition = Vector2.zero;
+
+                return;
+            }
+
+            if (IsIso)
+            {
+                _serialDisposable.Disposable.Dispose();
+
+                _cardPrefabParent.SetParent(_gridHolder, true);
+
+                _cardAnimator.IsHand = false;
+
+                _rectTransform.position = _buildRaycaster.CameraWorldToScreenPoint(_targetPosition.position);
+            }
+            else
+            {
+                _serialDisposable.Disposable.Dispose();
+
+                _cardPrefabParent.SetParent(_cardHolder, true);
+
+                _cardAnimator.IsHand = true;
+                Deselect(_currentCellSelectable);
+
+                _rectTransform.anchoredPosition = Vector2.zero;
+            }
         }
 
         private void Select(BuildCell selectable)
@@ -89,6 +173,27 @@ namespace Assets.Scripts.UI.Selectables.Cards
 
             selectable.Deselect();
             _currentCellSelectable = null;
+        }
+
+        public bool IsRaycastCardHolder(Vector2 position)
+        {
+            _results.Clear();
+
+            var pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = position
+            };
+
+            _raycaster.Raycast(pointerData, _results);
+
+            foreach (RaycastResult result in _results)
+            {
+                var resultObject = result.gameObject;
+
+                if (resultObject == _cardHolderGameObject) return true;
+            }
+
+            return false;
         }
     }
 }
